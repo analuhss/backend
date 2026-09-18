ed109d061f3db14c1f64fb1cdc8cc80bf5c1d0ef
Ana Luiza <anaguimaraes041@gmail.com>
2026-09-18 12:00:29 -0300

    conceitos gerais

--- AULA02/conceito_tipos_de_dados.js
+++ AULA02/conceito_tipos_de_dados.js
@@ -9,3 +9,42 @@
 // Array (Lista de valores)
 // NUl (valor vazio)
 // Undefined (quando algo não foi definifo)
+
+// STRING (Textos)
+// uma string é um texto, sempre escrito entre aspas ("" ou '')
+let nome = "Mega Brain";
+//a string já é definid pela aspas
+
+let mensagem = "Olá mundo!"
+
+console.log(nome);
+console.log(mensagem);
+
+let saudacao = "Olá," + nome + "!";
+console.log(saudacao); //Exibe olá mega brain
+
+// TYPEOF
+// O typeof serve para descobrir o tipo de um valor ou variável
+
+let nomeDois = "Bryan";
+console.log(typeof nomeDois);
+
+//____________________________________________________________________
+
+let soma = 10 + 5;
+console.log(soma)
+
+// Boolean (Verdadeiro ou falso)
+// um boolean pode ter apenas dois valores: true (verdadeiro) ou false (falso)
+
+let maiorDeIdade = true;
+let menorDeIdade = false;
+
+console.log(maiorDeIdade); //Exibe true
+console.log(menorDeIdade); //Exibe false
+
+let idade = 10;
+
+let podeDirigir = idade >= 18;
+
+console.log(podeDirigir)
---
+++ AULA02/exercicio01.js
@@ -0,0 +1,16 @@
+// ATIVIDADE NUMBER
+
+// Crie uma variável chamada anoAtual e outra chamada anoNascimento.
+// calcule e exibe a idade na console.
+
+// let anoAtual = 2026;
+
+// let anoNascimento = 2009;
+
+// let idade = anoAtual - anoNascimento;
+
+console.log(idade);
+
+// Template String
+console.log(`Sua idade é: ${idade} anos.`);
+
